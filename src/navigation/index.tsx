4dbe5f820dabafe105b57af39825b562be3f1516
Rodrigo Cibils <11689014+rodcibils@users.noreply.github.com>
2025-06-23 17:25:03 -0300

    Updated template values
    Added vscode settings
    
    Renamed existing screens and updated tab icons
    
    Removed example views

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,49 +1,30 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HeaderButton, Text } from '@react-navigation/elements';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   createStaticNavigation,
   StaticParamList,
-} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Image } from 'react-native';
-import bell from '../assets/bell.png';
-import newspaper from '../assets/newspaper.png';
-import { Home } from './screens/Home';
-import { Profile } from './screens/Profile';
-import { Settings } from './screens/Settings';
-import { Updates } from './screens/Updates';
-import { NotFound } from './screens/NotFound';
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Inbox } from "./screens/Inbox";
+import { NotFound } from "./screens/NotFound";
+import { Send } from "./screens/Send";
 
 const HomeTabs = createBottomTabNavigator({
   screens: {
-    Home: {
-      screen: Home,
+    Inbox: {
+      screen: Inbox,
       options: {
-        title: 'Feed',
+        title: "Inbox",
         tabBarIcon: ({ color, size }) => (
-          <Image
-            source={newspaper}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
+          <MaterialIcons name="inbox" size={size} color={color} />
         ),
       },
     },
-    Updates: {
-      screen: Updates,
+    Send: {
+      screen: Send,
       options: {
         tabBarIcon: ({ color, size }) => (
-          <Image
-            source={bell}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
+          <MaterialIcons name="send" size={size} color={color} />
         ),
       },
     },
@@ -55,40 +36,17 @@ const RootStack = createNativeStackNavigator({
     HomeTabs: {
       screen: HomeTabs,
       options: {
-        title: 'Home',
+        title: "Inbox",
         headerShown: false,
       },
-    },
-    Profile: {
-      screen: Profile,
-      linking: {
-        path: ':user(@[a-zA-Z0-9-_]+)',
-        parse: {
-          user: (value) => value.replace(/^@/, ''),
-        },
-        stringify: {
-          user: (value) => `@${value}`,
-        },
-      },
-    },
-    Settings: {
-      screen: Settings,
-      options: ({ navigation }) => ({
-        presentation: 'modal',
-        headerRight: () => (
-          <HeaderButton onPress={navigation.goBack}>
-            <Text>Close</Text>
-          </HeaderButton>
-        ),
-      }),
     },
     NotFound: {
       screen: NotFound,
       options: {
-        title: '404',
+        title: "404",
       },
       linking: {
-        path: '*',
+        path: "*",
       },
     },
   },
---
+++ src/navigation/screens/Send.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@react-navigation/elements";
+import { StyleSheet, View } from "react-native";
+
+export function Send() {
+  return (
+    <View style={styles.container}>
+      <Text>Placeholder</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
