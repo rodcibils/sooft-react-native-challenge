6ffeb6c5c518112bb3b5596b7981ffc2b3107f8e
Rodrigo Cibils <11689014+rodcibils@users.noreply.github.com>
2025-06-24 10:39:59 -0300

    Fixed notifee build issue on Android

---
+++ plugins/withNotifeeAndroidFix.js
@@ -0,0 +1,32 @@
+/**
+ * Needed to fix Notifee Android build that is broken as of now
+ *
+ * src:
+ * https://github.com/invertase/notifee/issues/1226
+ */
+const { withProjectBuildGradle } = require("@expo/config-plugins");
+const generateCode = require("@expo/config-plugins/build/utils/generateCode");
+
+const notifeeAndroidWorkaroundCode = `
+    maven { 
+        url "$rootDir/../node_modules/@notifee/react-native/android/libs" 
+    }
+`;
+
+/** @type {import('expo/config-plugins').ConfigPlugin} */
+module.exports = (expoConfig) => {
+  return withProjectBuildGradle(expoConfig, async (config) => {
+    const { contents } = generateCode.mergeContents({
+      newSrc: notifeeAndroidWorkaroundCode,
+      tag: "notifieeAndroidWorkaround",
+      src: config.modResults.contents,
+      anchor: /maven\s*\{\s*url\s*'https:\/\/www\.jitpack\.io'\s*\}/,
+      comment: "//",
+      offset: 1,
+    });
+
+    config.modResults.contents = contents;
+
+    return config;
+  });
+};
